a29e2b6ae9618031c50719c84fbaed4d1bf602e9
flague <flaviaguellafg@gmail.com>
2024-08-21 15:42:44 +0200

    Update nemo reference and utils

--- riscv_characterization/nemo_training_quantization/NEMO_quantization.py
+++ riscv_characterization/nemo_training_quantization/NEMO_quantization.py
@@ -3,13 +3,15 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import transforms
-import riscv_characterization.vendor.nemo
+import riscv_characterization.vendor.nemo as nemo
 import numpy as np
 from pathlib import Path
 import json
 import onnx
 import netron
 from packaging import version
+from neural_networks.MNIST.mnist_net import MNIST_model1, precision_std
+import nemo_training_quantization.utils as nemo_utils
 
 # model pth to be loaded and quantized
 model_name = "model1_48101624_75333_softmax_nobias_lr3e-3step035_wd1e-3_bs32_ep30_avg0.5"
@@ -43,8 +45,8 @@ print(model)
 loss_fn = nn.NLLLoss()  # result is the same using this or CrossEntropy if softmax is in the model
 
 
-_ = my_utils.test(test_dataloader, model, loss_fn, device, False )
-my_utils.print_acc(test_data, test_dataloader, model, device, False)
+_ = nemo_utils.test(test_dataloader, model, loss_fn, device, False )
+nemo_utils.print_acc(test_data, test_dataloader, model, device, False)
 
 
 print ("Go to NEMO fake quantized model")
@@ -53,18 +55,18 @@ nemo_model = nemo.transform.quantize_pact(model, dummy_input=dummy_input)
 print(nemo_model)
 
 nemo_model.change_precision(bits=1, min_prec_dict=precision_std)
-_ = my_utils.test(test_dataloader, nemo_model, loss_fn, device, False)
-my_utils.print_acc(test_data, test_dataloader, nemo_model, device, False)
+_ = nemo_utils.test(test_dataloader, nemo_model, loss_fn, device, False)
+nemo_utils.print_acc(test_data, test_dataloader, nemo_model, device, False)
 
 print(nemo_model)
 
 # adjust alpha value, according to statistics
 with nemo_model.statistics_act():
-    _ = my_utils.test(test_dataloader, nemo_model, loss_fn, device, False)
+    _ = nemo_utils.test(test_dataloader, nemo_model, loss_fn, device, False)
 print("Reset alpha act")
 nemo_model.reset_alpha_act()
-_ = my_utils.test(test_dataloader, nemo_model, loss_fn, device, False)
-my_utils.print_acc(test_data, test_dataloader, nemo_model, device, False)
+_ = nemo_utils.test(test_dataloader, nemo_model, loss_fn, device, False)
+nemo_utils.print_acc(test_data, test_dataloader, nemo_model, device, False)
 
 print(nemo_model)
 
@@ -75,15 +77,15 @@ nemo_model = nemo.transform.bn_to_identity(nemo_model) # necessary because foldi
 
 print ("Performing quantized deployable transformations")
 nemo_model.qd_stage(eps_in=1./255)
-_ = my_utils.test(test_dataloader, nemo_model, loss_fn, device, False)
-my_utils.print_acc(test_data, test_dataloader, nemo_model, device, False)
+_ = nemo_utils.test(test_dataloader, nemo_model, loss_fn, device, False)
+nemo_utils.print_acc(test_data, test_dataloader, nemo_model, device, False)
 
 
 print ("Performing integer deployable transformations")
 nemo_model.id_stage()
 #print(nemo_model)
-_ = my_utils.test(test_dataloader, nemo_model, loss_fn, device, integer=True)
-my_utils.print_acc(test_data, test_dataloader, nemo_model, device, integer = True)
+_ = nemo_utils.test(test_dataloader, nemo_model, loss_fn, device, integer=True)
+nemo_utils.print_acc(test_data, test_dataloader, nemo_model, device, integer = True)
 
 # save model state dict
 batch_size = bs
@@ -99,17 +101,17 @@ torch.save({ 'model_state_dict': nemo_model.state_dict(),
         }, PATH)
 
 # save info on PACT activations to be able to completely reload the model
-my_utils.write_act_parameters(nemo_model, act_json)
+nemo_utils.write_act_parameters(nemo_model, act_json)
 
 #write file with all intermediate results
 in_t = (torch.randint(low=0, high=256, size=(1,1,28,28), dtype=torch.float32)).to(device)
-my_utils.write_intermidiate_results(nemo_model, in_t, ex_dir)
-my_utils.write_weights(nemo_model,ex_dir)
+nemo_utils.write_intermidiate_results(nemo_model, in_t, ex_dir)
+nemo_utils.write_weights(nemo_model,ex_dir)
 
 
 # Save and modify onnx model
 #nemo.utils.export_onnx(onnx_id_name, nemo_model, nemo_model, (1,28,28), device)
 #onnx_model = onnx.load(onnx_id_name)
-#my_utils.rename_edges_onnx(onnx_model, onnx_id_name)
+#nemo_utils.rename_edges_onnx(onnx_model, onnx_id_name)
 
 
