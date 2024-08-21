c1676956c1ba41ade9dc4b6046d741a3e51524ae
flague <flaviaguellafg@gmail.com>
2024-08-21 16:06:40 +0200

    Add hook and change function (to check for nested models)

--- benchmark_CIFAR10/Utils/GA_utils.py
+++ benchmark_CIFAR10/Utils/GA_utils.py
@@ -5,9 +5,17 @@ import random
 from neural_networks.adapt.approx_layers import axx_layers as approxNN
 from neural_networks.CIFAR10.resnet import ResidualModule
 from neural_networks.utils import evaluate_test_accuracy
-import riscv_characterization.vendor.nemo
+import riscv_characterization.vendor.nemo as nemo
+import riscv_characterization.nemo_training_quantization.utils as nemo_utils
 
 # TODO: controllare dependencies e isinstance che ci sia nemo dappertutto
+
+activation={}
+def get_activation(name):
+    def hook(model, input, output):
+        activation[name] = output.detach()
+    return hook
+
 def encode_chromosome(model, n_appr_levels = 255, axx_linear=False):
     """
     Return the number of Conv layers in the model, that is the core info to encode chromosome
@@ -46,52 +54,52 @@ def initialize_population(Np, n_levels, n_layers):
     return population
 
 
-def list_mult_per_layer(model, imsize=(1,3,32,32), axx_linear=False):
+def list_mult_per_layer(model, imsize=(1, 3, 32, 32), axx_linear=False):
     """
-    @param model: torch.nn.module
+    @param model: torch.nn.Module
     @param imsize: tuple
-    @return:
+    @param axx_linear: bool
+    @return: list of multiplication counts per layer
     """
     model_mult = []
-    in_t = (torch.randint(low=0, high=256, size=imsize, dtype=torch.float32))
-    in_t.to('cpu')
+
+    # Register hooks for all relevant layers
+    for name, module in model.named_modules():
+        if isinstance(module, (nn.Conv2d, approxNN.AdaPT_Conv2d)):
+            module.register_forward_hook(get_activation(name))
+        elif axx_linear and isinstance(module, (nn.Linear, approxNN.AdaPT_Linear)):
+            module.register_forward_hook(get_activation(name))
+
+    # Perform a forward pass to trigger the hooks and capture activations
     model.eval()
     with torch.no_grad():
-        out = model(in_t)
-    for name, module in model._modules.items():
-        # if isinstance(module, nn.Conv2d) or isinstance(module, approxNN.AdaPT_Conv2d) or isinstance(module, nemo.quant.pact.PACT_Conv2d):
-        if isinstance(module, nn.Conv2d) or isinstance(module, approxNN.AdaPT_Conv2d):
+        in_t = torch.randint(low=0, high=256, size=imsize, dtype=torch.float32)
+        _ = model(in_t)
+
+    # Calculate multiplications for each relevant layer
+    for name, module in model.named_modules():
+        if isinstance(module, (nn.Conv2d, approxNN.AdaPT_Conv2d)):
             wgt = module.weight.shape
-            act = module.data_shape
+            act = activation[name].shape
             mult = wgt[0] * wgt[1] * wgt[2] * wgt[3] * act[2] * act[3]
             model_mult.append(mult)
-
-        elif isinstance(module, nn.Sequential):
-            for sub_name, sub_module in module._modules.items():
-                if isinstance(sub_module, ResidualModule):
-                    for subsub_name, subsub_module in sub_module._modules.items():
-                        # if isinstance(subsub_module, nn.Conv2d) or isinstance(subsub_module, approxNN.AdaPT_Conv2d) or isinstance(module, nemo.quant.pact.PACT_Conv2d):
-                        if isinstance(subsub_module, nn.Conv2d) or isinstance(subsub_module, approxNN.AdaPT_Conv2d):
-                            wgt = subsub_module.weight.shape
-                            act = subsub_module.data_shape
-                            mult = wgt[0] * wgt[1] * wgt[2] * wgt[3] * act[2] * act[3]
-                            model_mult.append(mult)
-        if axx_linear:
-            if isinstance(module, nn.Linear) or isinstance(module, approxNN.AdaPT_Linear):
-                wgt = module.weight.shape
-                mult = wgt[0] * wgt[1]
-                model_mult.append(mult)
+        elif axx_linear and isinstance(module, (nn.Linear, approxNN.AdaPT_Linear)):
+            wgt = module.weight.shape
+            act = activation[name].shape
+            mult = wgt[0] * act[0]  # Note: You can adjust this depending on your exact needs
+            model_mult.append(mult)
 
     return model_mult
 
 
-def evaluate_accuracy(model, train_data, test_data, train_args, test_only=False, partial=False, partial_val=10, nemo=False, json_file=None):
+def evaluate_accuracy(model, train_data, test_data, train_args, test_only=False, partial=False, partial_val=10, nemo=False, device='cpu'):
     """
     Simulate the network with the given configuration and evaluate its accuracy on the test dataset.
     At each call the model is re-loaded from the beginning
     """
     if not test_only or not nemo:
         loss_fn = nn.CrossEntropyLoss()  # result is the same using this or CrossEntropy if softmax is in the model
+        #torch.set_grad_enabled(True)
         model.train()
         opt = torch.optim.SGD(model.parameters(), lr=train_args['lr_max'], weight_decay=train_args['weight_decay'], momentum=train_args['lr_momentum'])
         lr_steps = int(len(train_data)/100.0*partial_val)
@@ -109,7 +117,7 @@ def evaluate_accuracy(model, train_data, test_data, train_args, test_only=False,
                     break
                 if partial and i < index:
                     continue
-                X, y = X.to('cpu'), y.to('cpu')
+                X, y = X.to(device), y.to(device)
                 output = model(X)
                 loss = loss_fn(output, y)
                 opt.zero_grad()
@@ -119,8 +127,12 @@ def evaluate_accuracy(model, train_data, test_data, train_args, test_only=False,
                 train_loss += loss.item() * y.size(0)
                 train_acc += (output.max(1)[1] == y).sum().item()
                 train_n += y.size(0)
-
-    test_loss, test_acc = evaluate_test_accuracy(test_data, model, 'cpu')
+    elif nemo:
+        loss_fn = nn.NLLLoss()
+        
+        test_acc = nemo_utils.test(test_data, model, loss_fn, device, True)
+    else:
+        test_loss, test_acc = evaluate_test_accuracy(test_data, model, device)
 
     return test_acc
 
